f7d66b7c5ebf4f81a68d4118f61fc004a1506d8d
Mayankmishra110 <kmmay15@gmail.com>
2024-05-15 23:42:31 +0530

    connect and design Appwrite services

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -55,4 +55,5 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 );
 Button.displayName = "Button";
 
-export { Button, buttonVariants };
+// export { Button, buttonVariants };
+export { Button };
--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,8 +1,8 @@
 import { INewUser } from "@/types";
 import { ID } from "appwrite";
-import { account } from "./config";
+import { account, appwriteConfig, avatars, databases } from "./config";
 
-// import { appwriteConfig, account, databases, storage, avatars } from "./config";
+// import { appwriteConfig, account,  storage } from "./config";
 // import { IUpdatePost, INewPost, INewUser, IUpdateUser } from "@/types";
 // import { INewUser, account  } from "@/types";
 
@@ -20,19 +20,19 @@ export async function createUserAccount(user: INewUser) {
       user.name
     );
 
-    // if (!newAccount) throw Error;
+    if (!newAccount) throw Error;
 
-    // const avatarUrl = avatars.getInitials(user.name);
+    const avatarUrl = avatars.getInitials(user.name);
 
-    // const newUser = await saveUserToDB({
-    //   accountId: newAccount.$id,
-    //   name: newAccount.name,
-    //   email: newAccount.email,
-    //   username: user.username,
-    //   imageUrl: avatarUrl,
-    // });
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      name: newAccount.name,
+      email: newAccount.email,
+      username: user.username,
+      imageUrl: avatarUrl,
+    });
 
-    return newAccount;
+    return newUser;
   } catch (error) {
     console.log(error);
     return error;
@@ -40,26 +40,26 @@ export async function createUserAccount(user: INewUser) {
 }
 
 // ============================== SAVE USER TO DB
-// export async function saveUserToDB(user: {
-//   accountId: string;
-//   email: string;
-//   name: string;
-//   imageUrl: URL;
-//   username?: string;
-// }) {
-//   try {
-//     const newUser = await databases.createDocument(
-//       appwriteConfig.databaseId,
-//       appwriteConfig.userCollectionId,
-//       ID.unique(),
-//       user
-//     );
+export async function saveUserToDB(user: {
+  accountId: string;
+  email: string;
+  name: string;
+  imageUrl: URL;
+  username?: string;
+}) {
+  try {
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user
+    );
 
-//     return newUser;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+    return newUser;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 // // ============================== SIGN IN
 // export async function signInAccount(user: { email: string; password: string }) {
